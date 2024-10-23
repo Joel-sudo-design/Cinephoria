f84eb68efcef58bc9867efcf3b5fa3ac581da298
Joel DERMONT <138232658+Joel-sudo-design@users.noreply.github.com>
2024-10-23 10:09:41 +0200

    Début Footer

--- assets/app.js
+++ assets/app.js
@@ -15,9 +15,29 @@ window.$ = window.jQuery = $;
 //Active Bootstrap
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 //Autoload images
 const imagesContext = require.context('../assets/images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
 imagesContext.keys().forEach(imagesContext);
 
+// Navbar
+// Lorsque le menu offcanvas est ouvert
+$(document).ready(function() {
+    $('#navbar-toggler').click(function() {
+        $('.offcanvas').css("background", "linear-gradient(90deg, rgba(106, 115, 171, 0.85) 50%, rgba(43, 46, 69, 0.85) 100%)");
+        $('#logo').hide();
+        $('#offcanvas-body').removeClass('row')
+        $('#navbarLeft').removeClass('col-5').addClass('col-12');
+        $('#navbarRight').removeClass('col-5').addClass('col-12');
+    });
+});
 
+// Lorsque le menu offcanvas est fermé
+$('#offcanvasNavbar').on('hidden.bs.offcanvas', function () {
+    $(this).css("background", "");
+    $('#logo').show();
+    $('#offcanvas-body').addClass('row')
+    $('#navbarLeft').removeClass('col-12').addClass('col-5');
+    $('#navbarRight').removeClass('col-12').addClass('col-5');
+});
