07538c29086cc839a45975434de47232d306e01c
Joel DERMONT <138232658+Joel-sudo-design@users.noreply.github.com>
2024-12-30 17:46:42 +0100

    Modif

--- src/Controller/CommandesController.php
+++ src/Controller/CommandesController.php
@@ -75,7 +75,68 @@ class CommandesController extends AbstractController
         ]);
 
     }
+    #[Route('/administrateur/mon_espace/commandes', name: 'app_commandes_admin')]
+    public function indexAdmin(EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $reservations = $user->getReservations();
+        $reservationArray = [];
 
+        foreach ($reservations as $reservation) {
+            $seance = [];
+            $cinemaId = $reservation->getCinema()->getId();
+            $cinema = $reservation->toArray();
+            $seance['seance'] = $reservation->getSeance()->toArray($cinemaId);
+
+            // Récupérer les heures de début et de fin
+            $heureDebut =  $reservation->getSeance()->getHeureDebut();
+            $heureFin = $reservation->getSeance()->getHeureFin();
+
+            if ($heureDebut && $heureFin) {
+                $diff = $heureDebut->diff($heureFin);
+
+                if ($diff->i === 0) {
+                    // Si les minutes sont 0, afficher uniquement les heures
+                    $duree = sprintf('%dh', $diff->h);
+                } else {
+                    // Sinon, afficher heures et minutes
+                    $duree = sprintf('%dh %dm', $diff->h, $diff->i);
+                }
+            } else {
+                $duree = null; // En cas de données incorrectes
+            }
+
+            $seance['seance']['duree'] = $duree;
+            $seance['seance']['cinema'] = $cinema['cinema'];
+            $seance['reservation_id'] = $reservation->getId();
+            $seance['seance']['sieges_reserves'] = $reservation->getSiege();
+            $seance['seance']['qrCode'] = $reservation->getQrCode();
+            $seance['film'] = $reservation->getSeance()->getFilm()->toArray();
+            $imageName = $reservation->getSeance()->getFilm()->getImageName();
+            $seance['film']['image'] = $imageName
+                ? $this->getParameter('films_images_directory') . '/image_film/' . $imageName
+                : null;
+            $genreName = $reservation->getSeance()->getFilm()->getGenre()->getName();
+            $seance['film']['genre'] = $genreName;
+            $Allavis = $reservation->getSeance()->getFilm()->getAvis()->toArray();
+            $seance['seance']['avis'] = [];
+            $seance['seance']['notation'] = [];
+            foreach ($Allavis as $avis) {
+                if ($avis->getUser()->getId() == $user->getId()) {
+                    $seance['seance']['avis'] = $avis->getDescription();
+                    $seance['seance']['notation'] = $avis->getNotation();
+                    break;
+                }
+            }
+            $reservationArray[] = $seance;
+        }
+
+        return $this->render('commandes/admin.html.twig', [
+            'controller_name' => 'CommandesAdminController',
+            'reservations' => $reservationArray,
+        ]);
+
+    }
     #[Route('/utilisateur/mon_espace/commandes/notation', name: 'app_commandes_user_notation')]
     public function notation(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
