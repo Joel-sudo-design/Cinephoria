9781124020c83d5f6737865e887c8e7595e8267d
Joel DERMONT <138232658+Joel-sudo-design@users.noreply.github.com>
2025-01-10 18:42:52 +0100

    Modif

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -177,7 +177,7 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('contact@joeldermont.fr', 'Mot de passe temporaire Cinéphoria'))
+            ->from(new Address('MAILER_FROM_ADDRESS', 'MAILER_FROM_MDP'))
             ->to((string) $user->getEmail())
             ->subject('Votre demande de réinitialisation de mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
